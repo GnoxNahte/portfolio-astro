71b6eb135ad0159fc2d85526d2d0001130773f1d
GnoxNahte <gnoxnahte.dev@gmail.com>
2023-01-29 17:47:21 +0800

    Add the base for the site
    Mostly followed this video as the base:
    https://www.youtube.com/watch?v=Oi9z5gfIHJs
    
    Added simple header, projects page, tags display, generating the static page for each project using markdown, some formatting for markdown.

---
+++ src/typescript/TagInfo.ts
@@ -0,0 +1,17 @@
+export enum Tags {
+    // Engine
+    Unity = 0,
+    Unreal,
+    Astro, 
+    
+    // Project Type
+    School = 100,
+    GameJam,
+    Personal,
+    Client,
+
+    // Platform
+    Windows = 200,
+    WebGL,
+    Website,
+}
