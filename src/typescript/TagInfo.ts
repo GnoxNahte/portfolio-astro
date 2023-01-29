31c176f9b2eba34f708415f05c62da8fff0c61a2
GnoxNahte <gnoxnahte.dev@gmail.com>
2023-01-29 20:32:31 +0800

    Minor changes
    - add horizontal line for project page
    - add comments on how to add new tags

--- src/typescript/TagInfo.ts
+++ src/typescript/TagInfo.ts
@@ -15,3 +15,9 @@ export enum Tags {
     WebGL,
     Website,
 }
+
+// To add a new tag, add it above in enum Tags.
+// If its in another catergory, add the tag and set it to last value + 100
+// Go to TagDisplay.astro and add it to 
+// - GetTagTypeClass() 
+// - CSS class
